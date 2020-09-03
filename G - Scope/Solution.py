28ff7fb9582d4d33019558a4125da4cf48a3af7a
Saurabh Chidrewar <68936847+chidrewarsaurabh@users.noreply.github.com>
2020-09-03 16:28:51 +0530

    Update Solution.py
    We can eliminate the for loop in the statement..

--- G - Scope/Solution.py
+++ G - Scope/Solution.py
@@ -3,16 +3,8 @@ class Difference:
         self.__elements = a
 
     def computeDifference(self):
-        maximum = 0
-
-        for i in range(len(self.__elements)):
-            for j in range(len(self.__elements)):
-                absolute = abs(self.__elements[i] - self.__elements[j])
-                if absolute > maximum:
-                    maximum = absolute
-
-        self.maximumDifference = maximum
-
+        s=max(a)-min(a)
+        self.maximumDifference = s
 
 _ = input()
 a = [int(e) for e in input().split(' ')]
